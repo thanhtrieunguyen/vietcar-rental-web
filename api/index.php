2af234dc19fdb51e8111408280d06177cdacd581
thanhtrieunguyen <121566700+thanhtrieunguyen@users.noreply.github.com>
2025-07-14 22:16:25 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,12 @@
 <?php
-require __DIR__ . "/../bootstrap/autoload.php";
-$app = require_once __DIR__ . "/../bootstrap/app.php";
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->call('config:clear');
 $kernel->call('cache:clear');
 $kernel->call('config:cache');
 
-require __DIR__ . "/../public/index.php";
+// Gọi entrypoint chính thức của Laravel
+require __DIR__ . '/../public/index.php';
