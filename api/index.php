12fccb1b696655af542415e4387df5d4d08eda07
thanhtrieunguyen <121566700+thanhtrieunguyen@users.noreply.github.com>
2025-07-14 22:12:05 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+require __DIR__ . "/../bootstrap/autoload.php";
+$app = require_once __DIR__ . "/../bootstrap/app.php";
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('config:clear');
+$kernel->call('cache:clear');
+$kernel->call('config:cache');
+
 require __DIR__ . "/../public/index.php";
